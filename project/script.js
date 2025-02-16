9102997d8163db7c454635087356b29903b1c04b
pedro_teixeira <pteixeira1089@hotmail.com>
2025-02-16 12:01:12 -0300

    Layout adjustments; pending report and cookies

--- project/script.js
+++ project/script.js
@@ -246,7 +246,7 @@ function loadScreen() {
         //Nome do júri elements        
         const juriNameLabel = document.createElement("label");
         juriNameLabel.for = "juriName";
-        juriNameLabel.classList.add("form-label");
+        juriNameLabel.classList.add("form-label", "h5");
         juriNameLabel.textContent = "Nome do júri";
 
         const juriNameInput = document.createElement("input");
@@ -258,6 +258,12 @@ function loadScreen() {
         juriNameInputTip.id = "juriNameHelp";
         juriNameInputTip.classList.add("form-text");
         juriNameInputTip.textContent = "Ex.: Ação Penal de competência do Júri n. 0000000-00.0000.4.03.6181";
+
+        const juriNameInputDiv = document.createElement("div");
+        juriNameInputDiv.classList.add("col-12", "mb-5");
+        juriNameInputDiv.appendChild(juriNameLabel);
+        juriNameInputDiv.appendChild(juriNameInput);
+        juriNameInputDiv.appendChild(juriNameInputTip);
 
 
         //Forma de sorteio elements
@@ -291,6 +297,11 @@ function loadScreen() {
 
 
         //Forma de sorteio divs
+        const formaSorteioDiv = document.createElement("div");
+        formaSorteioDiv.classList.add("col-12", "mb-5");
+        formaSorteioDiv.appendChild(formaSorteioLabel);
+        formaSorteioDiv.appendChild(formaSorteioRadioControl);
+
         const allAtOnceRadioDiv = document.createElement("div");
         allAtOnceRadioDiv.classList.add("col-12", "mb-1");
 
@@ -306,22 +317,30 @@ function loadScreen() {
         formaSorteioRadioControl.appendChild(oneNamePerClickRadioDiv);
 
 
-        //Quantiade de Jurados elements
+        //Quantidade de jurados - título
+        const quantidadeJuradosTitleDiv = document.createElement("div");
+        quantidadeJuradosTitleDiv.classList.add("col-12", "ms-0", "p-0", "mb-1");
+        
         const quantidadeJurados = document.createElement("h5");
         quantidadeJurados.textContent = "Quantidade de jurados";
+        quantidadeJuradosTitleDiv.appendChild(quantidadeJurados);
 
+        //Quantidade de jurados - form control
         const quantidadeJuradosFormControl = document.createElement("div");
-        quantidadeJurados.classList.add("form-check", "mb-3");
+        quantidadeJuradosFormControl.classList.add("col-12", "mb-5", "quantidade-jurados-form-control", "d-flex", "flex-wrap");
 
-        const InputQuantidadeJuradosTitulares = document.createElement("input");
-        InputQuantidadeJuradosTitulares.type = "number";
-        InputQuantidadeJuradosTitulares.id = "quantidadeJuradosTitulares";
-        InputQuantidadeJuradosTitulares.name = "quantidadeJuradosTitulares";
-        InputQuantidadeJuradosTitulares.min = "1"
-        InputQuantidadeJuradosTitulares.max = totalJuradosAlistados - 1;
+        //Quantidade de jurados - titulares e suplentes
+        const inputQuantidadeJuradosTitulares = document.createElement("input");
+        inputQuantidadeJuradosTitulares.classList.add("form-control", "col-3");
+        inputQuantidadeJuradosTitulares.type = "number";
+        inputQuantidadeJuradosTitulares.value = "25"
+        inputQuantidadeJuradosTitulares.id = "quantidadeJuradosTitulares";
+        inputQuantidadeJuradosTitulares.name = "quantidadeJuradosTitulares";
+        inputQuantidadeJuradosTitulares.min = "1"
+        inputQuantidadeJuradosTitulares.max = totalJuradosAlistados - 1;
 
         // Function to update the max value of quantidadeJuradosSuplentes
-        InputQuantidadeJuradosTitulares.addEventListener("input", function () {
+        inputQuantidadeJuradosTitulares.addEventListener("input", function () {
             const titulares = parseInt(this.value) || 0;
 
             inputQuantidadeJuradosSuplentes.max = totalJuradosAlistados - titulares;
@@ -336,11 +355,13 @@ function loadScreen() {
 
         const quantidadeJuradosTitularesLabel = document.createElement("label");
         quantidadeJuradosTitularesLabel.htmlFor = "quantidadeJuradosTitulares";
-        quantidadeJuradosTitularesLabel.classList.add("form-check-label");
-        quantidadeJuradosTitularesLabel.textContent = "Quantidade de jurados titulares";
+        quantidadeJuradosTitularesLabel.classList.add("form-check-label", "mr-2", "col-6");
+        quantidadeJuradosTitularesLabel.textContent = "Quantidade de jurados titulares: ";
 
         const inputQuantidadeJuradosSuplentes = document.createElement("input");
+        inputQuantidadeJuradosSuplentes.classList.add("col-3", "form-control");
         inputQuantidadeJuradosSuplentes.type = "number";
+        inputQuantidadeJuradosSuplentes.value = "25"
         inputQuantidadeJuradosSuplentes.id = "quantidadeJuradosSuplentes";
         inputQuantidadeJuradosSuplentes.name = "quantidadeJuradosSuplentes";
         inputQuantidadeJuradosSuplentes.min = "1"
@@ -348,31 +369,32 @@ function loadScreen() {
 
         const quantidadeJuradosSuplentesLabel = document.createElement("label");
         quantidadeJuradosSuplentesLabel.htmlFor = "quantidadeJuradosSuplentes";
-        quantidadeJuradosSuplentesLabel.classList.add("form-check-label");
-        quantidadeJuradosSuplentesLabel.textContent = "Quantidade de jurados suplentes";
-
-
-        //Quantidade de jurados divs
+        quantidadeJuradosSuplentesLabel.classList.add("form-check-label", "mr-2", "col-6");
+        quantidadeJuradosSuplentesLabel.textContent = "Quantidade de jurados suplentes: ";
+        
         const quantidadeJuradosTitularesDiv = document.createElement("div");
-        quantidadeJuradosTitularesDiv.classList.add("col-6", "mb-1");
+        quantidadeJuradosTitularesDiv.classList.add("mb-1", "d-flex", "align-items-center", "mr-3");
 
         const quantidadeJuradosSuplentesDiv = document.createElement("div");
-        quantidadeJuradosSuplentesDiv.classList.add("col-6", "mb-1");
+        quantidadeJuradosSuplentesDiv.classList.add("mb-1", "d-flex", "align-items-center", "mr-3");
+
+        const quantidadeJuradosInputsDiv = document.createElement("div");
+        quantidadeJuradosInputsDiv.classList.add("col-12", "d-flex", "flex-wrap");
 
         quantidadeJuradosTitularesDiv.appendChild(quantidadeJuradosTitularesLabel);
-        quantidadeJuradosTitularesDiv.appendChild(InputQuantidadeJuradosTitulares);
+        quantidadeJuradosTitularesDiv.appendChild(inputQuantidadeJuradosTitulares);
+
+        quantidadeJuradosInputsDiv.appendChild(quantidadeJuradosTitularesDiv);
+        quantidadeJuradosInputsDiv.appendChild(quantidadeJuradosSuplentesDiv);
 
         quantidadeJuradosSuplentesDiv.appendChild(quantidadeJuradosSuplentesLabel);
         quantidadeJuradosSuplentesDiv.appendChild(inputQuantidadeJuradosSuplentes);
 
-        quantidadeJuradosFormControl.appendChild(quantidadeJuradosTitularesDiv);
-        quantidadeJuradosFormControl.appendChild(quantidadeJuradosSuplentesDiv);
+        quantidadeJuradosFormControl.appendChild(quantidadeJuradosTitleDiv);
+        quantidadeJuradosFormControl.appendChild(quantidadeJuradosInputsDiv);
 
-        configurationForm.appendChild(juriNameLabel);
-        configurationForm.appendChild(juriNameInput);
-        configurationForm.appendChild(juriNameInputTip);
-        configurationForm.appendChild(formaSorteioLabel);
-        configurationForm.appendChild(formaSorteioRadioControl);
+        configurationForm.appendChild(juriNameInputDiv);
+        configurationForm.appendChild(formaSorteioDiv);
         configurationForm.appendChild(quantidadeJuradosFormControl);
 
 
@@ -415,7 +437,7 @@ function loadScreen() {
 
             nomeJuri = juriNameInput.value;
             formaSorteio = allAtOnceRadio.checked ? "allAtOnce" : "onePerClick";
-            quantidadeJuradosTitulares = parseInt(InputQuantidadeJuradosTitulares.value) || 0;
+            quantidadeJuradosTitulares = parseInt(inputQuantidadeJuradosTitulares.value) || 0;
             quantidadeJuradosSuplentes = parseInt(inputQuantidadeJuradosSuplentes.value) || 0;
 
             screenControl = 3; // Update screenControl
@@ -452,17 +474,17 @@ function loadScreen() {
 
     if (screenControl == 3) {
         clearScreen(); // Clear the screen before generating new elements
-
+    
         // Add your logic for screenControl == 3 here
         const horizontalRule = document.createElement("hr");
         const title = document.createElement("h3");
         title.classList.add("mb-4");
         title.textContent = "Certidões (CPP, art. 432)";
-
+    
         const paragraph = createParagraph("Certifico que foram intimados a acompanhar o processo de sorteio representantes dos seguintes órgãos e entidades:");
-
+    
         const form = document.createElement("form");
-
+    
         const checkboxes = [
             { id: "mpf", label: "Ministério Público Federal" },
             { id: "assistenteAcusacao", label: "Assistente de acusação" },
@@ -470,7 +492,7 @@ function loadScreen() {
             { id: "dpu", label: "Defensoria Pública da União" },
             { id: "defesaConstituida", label: "Defesa constituída" }
         ];
-
+    
         const checkboxStates = {
             mpf: false,
             assistenteAcusacao: false,
@@ -478,105 +500,105 @@ function loadScreen() {
             dpu: false,
             defesaConstituida: false
         };
-
+    
         checkboxes.forEach(({ id, label }) => {
             const div = document.createElement("div");
-            div.classList.add("form-check", "mb-2");
-
+            div.classList.add("form-check", "mb-4");
+    
             const input = document.createElement("input");
             input.type = "checkbox";
             input.classList.add("form-check-input");
             input.id = id;
             input.name = id;
-
+    
             input.addEventListener("change", (event) => {
                 checkboxStates[id] = event.target.checked;
                 checkAllSelected();
             });
-
+    
             const inputLabel = document.createElement("label");
             inputLabel.classList.add("form-check-label");
             inputLabel.htmlFor = id;
             inputLabel.textContent = label;
-
+    
             div.appendChild(input);
             div.appendChild(inputLabel);
             form.appendChild(div);
         });
-
+    
         function checkAllSelected() {
             const allSelected = Object.values(checkboxStates).every(Boolean);
             realizarSorteioButton.disabled = !allSelected;
         }
-
+    
         // Action buttons
         const realizarSorteioButton = document.createElement("button");
         realizarSorteioButton.classList.add("btn", "btn-primary", "mb-3");
         realizarSorteioButton.textContent = "Realizar sorteio";
         realizarSorteioButton.disabled = true;
-
+    
         const voltarButton = document.createElement("button");
         voltarButton.classList.add("btn", "btn-secondary", "mb-3");
         voltarButton.textContent = "Voltar: alterar configurações de sorteio";
-
+    
         realizarSorteioButton.addEventListener("click", (event) => {
             event.preventDefault();
-
+    
             // Update global variables
             Object.keys(checkboxStates).forEach(key => {
                 window[key] = checkboxStates[key];
             });
-
+    
             // Proceed to the next screen or perform the draw
             screenControl = 4;
             loadScreen(); // Reload the screen if needed
         });
-
+    
         voltarButton.addEventListener("click", (event) => {
             event.preventDefault();
             screenControl = 2; // Update screenControl
             loadScreen(); // Reload the screen
         });
-
+    
         const backRow = document.createElement("div");
         backRow.classList.add("row", "action-row");
-
+    
         const backCol = document.createElement("div");
         backCol.classList.add("col-12");
-
+    
         const nextRow = document.createElement("div");
         nextRow.classList.add("row", "action-row");
-
+    
         const nextCol = document.createElement("div");
         nextCol.classList.add("col-12");
-
+    
         backCol.appendChild(voltarButton);
         backRow.appendChild(backCol);
-
+    
         nextCol.appendChild(realizarSorteioButton);
         nextRow.appendChild(nextCol);
-
+    
         actionDiv.appendChild(nextRow);
         actionDiv.appendChild(backRow);
-
+    
         // Page building
         const titleRow = document.createElement("div");
-        titleRow.classList.add("row", "text-row");
-
+        titleRow.classList.add("row", "text-row", "justify-content-center");
+    
         const titleCol = document.createElement("div");
-        titleCol.classList.add("col-12");
-
+        titleCol.classList.add("col-12", "text-center");
+    
         titleCol.appendChild(horizontalRule);
         titleCol.appendChild(title);
         titleRow.appendChild(titleCol);
         contentDiv.appendChild(titleRow);
-
+    
         const contentRow = document.createElement("div");
-        contentRow.classList.add("row", "text-row", "text-left");
-
+        contentRow.classList.add("row", "text-row", "justify-content-center");
+    
         const contentCol = document.createElement("div");
-        contentCol.classList.add("col-12");
-
+        contentCol.classList.add("col-10", "text-center");
+    
         contentCol.appendChild(paragraph);
         contentCol.appendChild(form);
         contentRow.appendChild(contentCol);
